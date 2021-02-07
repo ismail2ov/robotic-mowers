836bee1744167d04a903f128929eb93a9de2d169
Ismail Ahmedov <ismail.ahmedov@build38.com>
2021-02-07 02:54:38 +0100

    Add Heading Orientation

---
+++ src/main/java/seat/code/robotic/mowers/domain/HeadingOrientation.java
@@ -0,0 +1,36 @@
+package seat.code.robotic.mowers.domain;
+
+public enum HeadingOrientation {
+    NORTH("N", "W", "E"),
+    EAST("E", "N", "S"),
+    SOUTH("S", "E", "W"),
+    WEST("W", "S", "N"),
+    UNDEFINED("U", "U", "U");
+
+    private final String current;
+    private final String left;
+    private final String right;
+
+    HeadingOrientation(final String current, final String left, final String right) {
+        this.current = current;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static HeadingOrientation from(final String heading) {
+        for (final HeadingOrientation headingOrientation : values()) {
+            if (headingOrientation.current.equals(heading)) {
+                return headingOrientation;
+            }
+        }
+        return HeadingOrientation.UNDEFINED;
+    }
+
+    public HeadingOrientation turnLeft() {
+        return HeadingOrientation.from(this.left);
+    }
+
+    public HeadingOrientation turnRight() {
+        return HeadingOrientation.from(this.right);
+    }
+}
---
+++ src/test/java/seat/code/robotic/mowers/domain/HeadingOrientationShould.java
@@ -0,0 +1,47 @@
+package seat.code.robotic.mowers.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class HeadingOrientationShould {
+
+    @Test
+    void whenHeadingOrientationIsLostThenReturnsUndefined() {
+        final HeadingOrientation headingOrientation = HeadingOrientation.from("Y");
+
+        assertThat(headingOrientation).isEqualTo(HeadingOrientation.UNDEFINED);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "N, W",
+        "E, N",
+        "S, E",
+        "W, S",
+    })
+    void shouldTurnLeft(final String current, final String expected) {
+        final HeadingOrientation headingOrientation = HeadingOrientation.from(current);
+
+        final HeadingOrientation actual = headingOrientation.turnLeft();
+
+        assertThat(actual).isEqualTo(HeadingOrientation.from(expected));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "N, E",
+        "E, S",
+        "S, W",
+        "W, N",
+    })
+    void shouldTurnRight(final String current, final String expected) {
+        final HeadingOrientation headingOrientation = HeadingOrientation.from(current);
+
+        final HeadingOrientation actual = headingOrientation.turnRight();
+
+        assertThat(actual).isEqualTo(HeadingOrientation.from(expected));
+    }
+}
